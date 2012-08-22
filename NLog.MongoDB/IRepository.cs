41bf3b109df7b59aec6fa2de6affd99fa1a04e76
thecutter <info@thecutter.net>
2012-08-22 10:27:14 +0200

    added capped collection support

--- NLog.MongoDB/IRepository.cs
+++ NLog.MongoDB/IRepository.cs
@@ -6,5 +6,6 @@ namespace NLog.MongoDB
 	public interface IRepository : IDisposable
 	{
 		void Insert(string collectionName, BsonDocument item);
+	    void CheckCollection(string collectionName, bool useCappedCollection, long? cappedCollectionSize, long? cappedCollectionMaxItems, bool createIdField);
 	}
 }
--- NLog.MongoDB/MongoRepository.cs
+++ NLog.MongoDB/MongoRepository.cs
@@ -3,34 +3,56 @@ using MongoDB.Driver;
 
 namespace NLog.MongoDB
 {
-	public class MongoRepository : IRepository
+    using System.Collections.Generic;
+    using global::MongoDB.Driver.Builders;
+
+    public class MongoRepository : IRepository
 	{
 		private MongoServer _Server;
 		private readonly string _Database;
+        private static readonly List<string> CollectionCache = new List<string>();
 
-		public MongoRepository(
-			MongoServerSettings settings,
-			string database)
-		{
-			_Server = new MongoServer(settings);
-            _Database = database;
-			_Server.Connect();
-		}
-
-        public MongoRepository(
-            string connectionString,
-            string database)
-        {
-            _Server = MongoServer.Create(connectionString);
-            _Database = connectionString.ParseDatabaseName() ?? database;
-            _Server.Connect();
+        public MongoRepository(MongoServerSettings settings, string databaseName)
+	    {
+            _Server = new MongoServer(settings);
+            _Database = databaseName;
+	        _Server.Connect();
         }
 
-		public void Insert(string collectionName, BsonDocument item)
+        public void CheckCollection(string collectionName, bool useCappedCollection, long? cappedCollectionSize, long? cappedCollectionMaxItems, bool createIdField)
+        {
+            if (useCappedCollection && !cappedCollectionSize.HasValue)
+            {
+                throw new NLogConfigurationException("If UseCappedCollection is set to true, you must set CappedCollectionSize!");
+            }
+
+            var db = _Server.GetDatabase(_Database);
+
+            lock (CollectionCache)
+            {
+                if (!useCappedCollection || CollectionCache.Contains(collectionName)) return;
+                
+                if (!db.CollectionExists(collectionName))
+                {
+                    var collectionOptionsBuilder = new CollectionOptionsBuilder();
+                    collectionOptionsBuilder.SetCapped(true);
+                        
+                    if (createIdField) collectionOptionsBuilder.SetAutoIndexId(true);
+                    if (cappedCollectionSize.HasValue) collectionOptionsBuilder.SetMaxSize(cappedCollectionSize.Value);
+                    if (cappedCollectionMaxItems.HasValue) collectionOptionsBuilder.SetMaxDocuments(cappedCollectionMaxItems.Value);
+
+                    db.CreateCollection(collectionName, collectionOptionsBuilder);
+                }
+
+                CollectionCache.Add(collectionName);
+            }
+
+        }
+
+	    public void Insert(string collectionName, BsonDocument item)
 		{
 			var db = _Server.GetDatabase(_Database);
-
-		    var collection = db.GetCollection(collectionName);
+	        var collection = db.GetCollection(collectionName);
 			collection.Insert(item);
 		}
 
