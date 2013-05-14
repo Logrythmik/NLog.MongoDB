2e909abc7341c7b607a9f008e595dbb764a32e61
Josh Gruenberg <jgruenberg@brandnetworksinc.com>
2013-05-14 11:58:51 -0400

    Added a pretty basic unit test, but a unit test nonetheless!

---
+++ NLog.MongoDB.Tests/UnitTests/TestMongoTargetDocument.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLog.Layouts;
+
+namespace NLog.MongoDB.Tests
+{
+    [TestClass]
+    public class TestMongoTargetDocument
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            const string name = "SomeName!";
+
+            var doc = new MongoDBTargetDocument();
+            doc.Name = name;
+
+            doc.Name
+                .Should().Be(name);
+            doc.Fields.Count
+                .Should().Be(0);
+            doc.Documents.Count
+                .Should().Be(0);
+        }
+    }
+}
